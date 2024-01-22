d7e909cc2f13cddb9615a97b99ef11a43ca8ab49
Vivek <35356837+ninjas005@users.noreply.github.com>
2024-01-22 12:54:02 +0530

    deep_sort

--- detect_v3.py
+++ detect_v3.py
@@ -12,9 +12,8 @@ cap = cv2.VideoCapture(0)
 while True:
     ret,frame = cap.read()
     
-    preds=model(frame, device=device, classes=[2,3,4], conf=0.3, iou=0.5)
+    preds = model(frame, device=device, classes=[2,3,4], conf=0.3, iou=0.5)
     # predictions = model.predict(frame, device=0, classes=0, save_crop=True)
-
 
     for pred in preds:
         img = pred.plot()
@@ -22,5 +21,6 @@ while True:
         
     cv2.imshow('PPE', np.array(img,dtype = np.uint8))
     if cv2.waitKey(1) & 0xff == ord('q'):
-    	break
+        break
+
 cap.release()
